4e694a03a8abc303f6aaa918e20e631be4d1b6f5
bvgaurav4 <bv.gaurav4@gmail.com>
2026-01-10 23:04:00 +0530

    auth start

--- Authentication/Readme.md
+++ Authentication/Readme.md
@@ -7,3 +7,9 @@ for this service we are using rust cause its thread safe and it dosent have a gu
         it checks if the user is registered and manages the session of the user and also handles the accessibility of the use (dosent allow to query data for another user)
 
         this is handeled by the token provided by the auth service.
+
+        using redis for stroing tokens os users and validating them
+
+
+
+        redis-cli ping
--- Authentication/src/main.rs
+++ Authentication/src/main.rs
@@ -1,16 +1,53 @@
-use reqwest;
-use std::collections::HashMap;
+use axum::{routing::get, Router, response::IntoResponse};
+use redis::Commands;
+// use redis::TypedCommands;
+
+async fn hello() -> Result<(), ()> {
+    // let client = redis::Client::open("redis://127.0.0.1/")
+    //     .map_err(|e| e.to_string())?;
+
+    // let mut conn = client
+    //     .get_connection()
+    //     .map_err(|e| e.to_string())?;
+
+    // conn.set("game:players", 10)
+    //     .map_err(|e| e.to_string())?;
+
+    // let players: i32 = conn
+    //     .get("game:players")
+    //     .map_err(|e| e.to_string())?;
+
+    // Ok(format!("Hello, players = {}", players))
+        let mut r = match redis::Client::open("redis://127.0.0.1") {
+            Ok(client) => {
+                match client.get_connection() {
+                    Ok(conn) => conn,
+                    Err(e) => {
+                        println!("Failed to connect to Redis: {e}");
+                        return Ok(());
+                    }
+                }
+            },
+            Err(e) => {
+                println!("Failed to create Redis client: {e}");
+                return Ok(());
+            }
+        };
+        return Ok(());
+
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, world!");    
-    let mut map = HashMap::new();
-    map.insert("lang", "rust");
-    map.insert("body", "json");
+async fn main() {
+    let app = Router::new()
+        .route("/hello", get(hello))
+        .route("/hello2", get(hello));
 
-    let response = reqwest::get("https://www.rust-lang.org").await?;
-    let body = response.text().await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:5000")
+        .await
+        .unwrap();
 
-    println!("body = {:?}", body);
-    Ok(())
+    axum::serve(listener, app)
+        .await
+        .unwrap();
 }
